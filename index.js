a2880059f5ef48a9d2d755f6aa0fbc92a34374a9
Danil Kamyshov <danil.kamyshov@gmail.com>
2018-05-13 13:57:30 +0300

    master: correct __esModule, removed 'seperable' flag

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var minify = require('pg-minify');
 module.exports = function (source) {
   this.cacheable && this.cacheable();
   var transformed = minify(source, { compress: true });
-  return "module.exports = { __esModule: true, default: " + JSON.stringify(transformed) + " }";
+  var result = "Object.defineProperty(exports, '__esModule', {value: true});\n";
+  result += "exports[\"default\"] = " + JSON.stringify(transformed) + ";";
+  return result;
 }
-module.exports.seperable = true;
